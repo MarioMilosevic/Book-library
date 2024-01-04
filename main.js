1410cdf3f4464c68ec99278be769f6ec84c9e869
Mario Milošević <mariomilosevic887@gmail.com>
2024-01-04 18:28:21 +0100

    Create book innerHTML

--- main.js
+++ main.js
@@ -9,4 +9,20 @@ const {
 newBtn.addEventListener('click', function(){
     modal.classList.toggle('hidden')
     overlay.classList.toggle('hidden')
+    console.log('newBtn');
+    console.log(modal);
+    console.log(overlay);
 })
+
+const createBook = () => {
+    const book = document.createElement('div')
+    book.innerHTML = ''
+    book.innerHTML = `<div class="book">
+        <p class="bookTitle"></p>
+        <p class="bookAuthorName"></p>
+        <p class="bookNumPages"></p>
+        <button class="read"></button>
+        <button class="removeBook"></button>
+    </div>`
+
+}

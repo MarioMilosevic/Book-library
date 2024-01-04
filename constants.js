b023cea4125f3d8807d47f802f68cb9080e849d3
Mario Milošević <mariomilosevic887@gmail.com>
2024-01-04 17:34:01 +0100

    Add constants js and on btn click show modal and overlay

---
+++ constants.js
@@ -0,0 +1,11 @@
+'use strict'
+
+export function data(){
+const newBtn = document.querySelector('.newBtn')
+const overlay = document.querySelector('.overlay')
+const modal = document.querySelector('.modal')
+
+return {
+    newBtn, modal, overlay
+}
+}
--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 'use strict'
+import { data } from "./constants"
+
+const {
+    newBtn, modal, overlay
+} = data()
+
+
+newBtn.addEventListener('click', function(){
+    modal.classList.toggle('hidden')
+    overlay.classList.toggle('hidden')
+})

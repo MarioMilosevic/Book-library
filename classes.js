c693cad2dd1aa1730e304df3e144a0655f47f96b
Mario Milošević <mariomilosevic887@gmail.com>
2024-01-04 18:38:29 +0100

    Add classes.js

---
+++ classes.js
@@ -0,0 +1,22 @@
+'use strict'
+
+class Book {
+constructor(title, author, number){
+    this.title = title
+    this.author = author
+    this.number = number
+}
+}
+
+class BookManager {
+constructor(){
+    this.bookCollection = []
+}
+
+
+
+}
+
+class UI {
+
+}
--- main.js
+++ main.js
@@ -1,28 +1,24 @@
-'use strict'
-import { data } from "./constants"
+"use strict";
+import { data } from "./constants";
 
-const {
-    newBtn, modal, overlay
-} = data()
+const { newBtn, modal, overlay } = data();
 
-
-newBtn.addEventListener('click', function(){
-    modal.classList.toggle('hidden')
-    overlay.classList.toggle('hidden')
-    console.log('newBtn');
-    console.log(modal);
-    console.log(overlay);
-})
+newBtn.addEventListener("click", function () {
+  modal.classList.toggle("hidden");
+  overlay.classList.toggle("hidden");
+  console.log("newBtn");
+  console.log(modal);
+  console.log(overlay);
+});
 
 const createBook = () => {
-    const book = document.createElement('div')
-    book.innerHTML = ''
-    book.innerHTML = `<div class="book">
+  const book = document.createElement("div");
+  book.innerHTML = "";
+  book.innerHTML = `<div class="book">
         <p class="bookTitle"></p>
         <p class="bookAuthorName"></p>
         <p class="bookNumPages"></p>
         <button class="read"></button>
         <button class="removeBook"></button>
-    </div>`
-
-}
+    </div>`;
+};
